f3afc33150db9007f289746e7329be4e7d65e60a
soz <aruta.soz@proton.me>
2023-01-16 23:06:14 +0100

    commit new functions made to get checksum and reconstitute files and checksum

--- hosting/src/index.ts
+++ hosting/src/index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import util from 'util';
+import stream from 'stream';
 import {authorityKeypair, collateralMint, payerKeypair, PROGRAM_ID} from "../../tests/constant";
 import {CHUNK_BYTE_SIZE, Frex} from '../../client/src/Frex';
 import {BN, Wallet} from "@project-serum/anchor";
@@ -14,7 +15,7 @@ import {
 } from "@solana/web3.js";
 import {SignerWallet} from '@saberhq/solana-contrib';
 import * as crypto from "crypto";
-import {Buffer, Domain} from '../../client/src/types';
+import {Buffer, BufferChunk, Domain} from '../../client/src/types';
 import * as domain from "domain";
 
 const connection = new Connection("https://api.devnet.solana.com", 'processed');
@@ -33,14 +34,54 @@ const frex = Frex.init({
 });
 
 async function loadBuffer(domainPubKey: PublicKey, bufferVersion: number) {
-    const bufferAddress = frex.findBufferAddress(domainPubKey, bufferVersion)
+    const bufferAddress: PublicKey = frex.findBufferAddress(domainPubKey, bufferVersion)
 
-    const buffer = await frex.frexProgram.account.buffer.fetchNullable(bufferAddress)
+    const buffer: Buffer = await frex.frexProgram.account.buffer.fetchNullable(bufferAddress)
     if (buffer === null)
         throw new Error(`buffer at address ${bufferAddress} is missing, what is happening ?`)
 
-    return buffer
+    console.log("buffer = " + buffer)
 
+    const checksum = buffer.checksum
+    const bufferChunkAddress: PublicKey = frex.findBufferChunkAddress(bufferAddress, buffer.chunkNumber.toNumber())
+    //open stream and put chunk inside during execution
+
+    console.log("bufferChunkAddress = " + bufferChunkAddress)
+
+    const bufferChunk: BufferChunk = await frex.frexProgram.account.bufferChunk.fetchNullable(bufferChunkAddress)
+
+    console.log("bufferChunk = " + bufferChunk)
+    const out = fs.createWriteStream('/tmp/testFile.txt');
+
+    out.write(bufferChunk.data.toString());
+    out.end();
+
+    const newFile = fs.readFileSync('/tmp/testFile.txt')
+
+    const checksumNewFile = Buffer.from(crypto.createHash('sha256')
+        .update(newFile.toString())
+        .digest('hex').toString());
+
+    const checksumAsHex = Array.from(checksum, function(byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+
+    //compare checksum
+    console.log("checksum = " + checksumAsHex)
+    console.log("checksumNewFile = " + checksumNewFile)
+
+    //dezip
+    return bufferChunk
+}
+
+async function loadBufferChunks(domainPubKey: PublicKey, chunk_number: number) {
+    const bufferChunkAddress = frex.findBufferChunkAddress(domainPubKey, chunk_number)
+
+    const bufferChunk = await frex.frexProgram.account.bufferChunk.fetchNullable(bufferChunkAddress)
+    if (bufferChunk === null)
+        throw new Error(`bufferChunk at address ${bufferChunkAddress} is missing, what is happening ?`)
+
+    return bufferChunk
 }
 
 (async () => {
@@ -49,8 +90,7 @@ async function loadBuffer(domainPubKey: PublicKey, bufferVersion: number) {
         dataSlice: {offset: 0, length: 0},
         filters: [{dataSize: 1047}]
     })
-    const domainsData = await frex.frexProgram.account.domain.fetchMultiple(domains.map(domain => domain.pubkey)) as Domain[]
-    console.log(domainsData)
+    const domainsData: Domain[] = await frex.frexProgram.account.domain.fetchMultiple(domains.map(domain => domain.pubkey)) as Domain[]
 
     const activeDomains = domainsData.reduce((accActiveDomains: { [key: string]: Domain }, domain: Domain, i: number) => {
         if (domain.activeBufferVersion.toNumber() === 0)
@@ -60,21 +100,13 @@ async function loadBuffer(domainPubKey: PublicKey, bufferVersion: number) {
         return accActiveDomains
     }, {} as { [key: string]: Domain; })
 
-    console.log(activeDomains)
     // generate buffer address
-    const buffers = await Promise.all(
-        Object.entries(activeDomains).map(([domainPubKey, domain]) => loadBuffer(new PublicKey(domainPubKey), domain.activeBufferVersion.toNumber()))
-    )
+    const buffers = await Promise.all(Object.entries(activeDomains).map(([domainPubKey, domain]) => loadBuffer(new PublicKey(domainPubKey), domain.activeBufferVersion.toNumber())))
 
     console.log(buffers)
 
-    //download buffer chunk
 
-    //open stream and put chunk inside during execution
 
-    //compare checksum
-
-    //dezip
 
     // add events in program (create instruction)
 
